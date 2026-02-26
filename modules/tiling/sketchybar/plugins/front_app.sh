60aa84bc80c3a851d5868a2903714856dcb836a7
Angel Eduardo Vega Conde <56698379+AEduardo-dev@users.noreply.github.com>
2026-02-26 11:45:28 +0100

    fix: app icons in front app plugin

--- modules/tiling/sketchybar/plugins/front_app.sh
+++ modules/tiling/sketchybar/plugins/front_app.sh
@@ -3,6 +3,7 @@
 # Sketchybar environment variables
 : "${INFO:=}"
 
+source "$CONFIG_DIR/core/env.sh"
 source "$CONFIG_DIR/tokens/colors.sh"
 source "$CONFIG_DIR/plugins/icon.sh"
 
@@ -10,6 +11,8 @@ if [ -n "$INFO" ]; then
   APP_NAME="$INFO"
   ICON=$(get_app_icon "$APP_NAME")
 
-  sketchybar --set front_app.icon icon="$ICON"
+  sketchybar --set front_app.icon \
+    icon="$ICON" \
+    icon.font="${SBAR_APP_ICON_FONT:-sketchybar-app-font}:Regular:${SBAR_APP_ICON_FONT_SIZE:-13.5}"
   sketchybar --set front_app.name label="$APP_NAME"
 fi
